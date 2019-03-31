e9e189b040ef9445ab670fa1f9a39e3a6a7104f2
aort93 <alonsojortega@gmail.com>
2019-03-30 22:21:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file.
+function lowerCaseDrivers(obj) {
+  return obj.map(function(driver) {
+    return driver.toLowerCase();
+  });
+}
+
+function nameToAttributes(obj) {
+  return obj.map(function(driver) {
+    name = driver.split(' ')
+    const first = name[0];
+    const last = name[1];
+
+    return { firstName: first, lastName: last};
+  });
+}
+
+function attributesToPhrase(obj) {
+  return obj.map(function(driver) {
+    return `${driver.name} is from ${driver.hometown}`;
+  });
+}
+
+
+const drivers = [
+  'Bobby Smith',
+  'Sammy Watkins',
+  'Sally Jenkins',
+  'Annette Sawyer',
+  'Sarah Hucklebee',
+  'bobby ridge'
+];
+
+console.log(nameToAttributes(drivers));
